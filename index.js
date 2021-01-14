2480bc3b73f71d75919fe66e0d64f28672d38c0c
Noor Albayaty <noor.albayaty90@gmail.com>
2021-01-14 14:33:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ var specialGuest = 'Neil deGrasse Tyson';
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = 'Let\'s talk about Let\'s talk about space.';
-var conversation =console.log(topic);
+var conversation =topic;
